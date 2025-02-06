e92ca69ef82766530bd12db5acee1ea4c79ebbfc
AryanTD <aryantandon285@gmail.com>
2025-02-06 16:34:30 -0700

    Initial Commit

---
+++ Calculator/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Calculator
+//
+//  Created by Aryan Tandon on 2/6/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
