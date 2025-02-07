c02af6061517486266c7dff8c2fdbcc8fa38f4b2
AryanTD <aryantandon285@gmail.com>
2025-02-06 17:43:46 -0700

    Organized into Functions

--- Calculator/main.cpp
+++ Calculator/main.cpp
@@ -7,6 +7,26 @@
 
 #include "tools.hpp"
 
+double add(double a, double b){
+    return a + b;
+}
+
+double subtraction(double a, double b){
+    return a - b;
+}
+
+double multiplication(double a, double b){
+    return a * b;
+}
+
+double division(double a, double b){
+    if (b != 0)
+        return a / b;
+    else
+        cout << "Error! Division by zero." << endl;
+    return 0;
+}
+
 int main(int argc, const char * argv[]) {
     
     double num1, num2;
@@ -24,17 +44,14 @@ int main(int argc, const char * argv[]) {
     double result;
     
     switch(op) {
-        case '+': result = num1 + num2; break;
-        case '-': result = num1 - num2; break;
-        case '*': result = num1 * num2; break;
-        case '/':
-            if (num2 != 0)
-                cout << "Result: " << num1 / num2 << endl;
-            else
-                cout << "Error! Division by zero." << endl;
-            break;
+        case '+': result = add(num1, num2); break;
+        case '-': result = subtraction(num1, num2); break;
+        case '*': result = multiplication(num1, num2); break;
+        case '/': result = division(num1, num2); break;
         default: cout << "Invalid operator!" << endl;
     }
+    
+    cout << "Result :" << result << endl;
     
     return 0;
 }
