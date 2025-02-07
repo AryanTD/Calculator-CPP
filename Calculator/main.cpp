2d28d19ca15519264efe7ee849c61759c023f554
AryanTD <aryantandon285@gmail.com>
2025-02-06 17:27:20 -0700

    Added Basic Arithmetic

--- Calculator/main.cpp
+++ Calculator/main.cpp
@@ -8,7 +8,33 @@
 #include "tools.hpp"
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    double num1, num2;
+    char op;
+    
+    cout << "Enter first numbers: ";
+    cin >> num1;
+    
+    cout << "Enter operator (+, -, *, /): ";
+    cin >> op;
+    
+    cout << "Enter second numbers: ";
+    cin >> num2;
+    
+    double result;
+    
+    switch(op) {
+        case '+': result = num1 + num2; break;
+        case '-': result = num1 - num2; break;
+        case '*': result = num1 * num2; break;
+        case '/':
+            if (num2 != 0)
+                cout << "Result: " << num1 / num2 << endl;
+            else
+                cout << "Error! Division by zero." << endl;
+            break;
+        default: cout << "Invalid operator!" << endl;
+    }
+    
     return 0;
 }
